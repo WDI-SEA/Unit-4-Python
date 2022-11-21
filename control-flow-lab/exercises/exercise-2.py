bc9d5fd1fa4988d52a1a084792b0923d491cae52
Fuad AlShuroogi <fuadalshuroogi@gmail.com>
2022-11-21 21:06:38 +0300

    Fuad AlShuroogi - Done With Control Flow Lab

--- control-flow-lab/exercises/exercise-2.py
+++ control-flow-lab/exercises/exercise-2.py
@@ -7,3 +7,19 @@
 #      - What you entered is xx characters long
 # 3. Return to step 1, unless the word 'quit' was entered.
 
+
+# while word_phrase != "quit":
+
+#     word_phrase = input('Please enter a word or phrase : ').lower()
+#     print(f"What you entered is {len(word_phrase)} characters long")
+
+word = " "
+
+while word != 'quit':
+
+    word = input('Please enter a word or phrase:')
+    if word == 'quit':
+        print('')
+    else:
+        print(f'What you entered is {len(word)} characters long')
+
--- control-flow-lab/exercises/exercise-4.py
+++ control-flow-lab/exercises/exercise-4.py
@@ -12,3 +12,18 @@
 #      isosceles - two sides are the same length
 # 3. Print a message such as:
 #      - A triangle with sides of <a>, <b> & <c> is a <type of triangle> triangle
+
+side_1 = int(input('Enter the lengths of three side of a triangle: \na:' ))
+side_2 = int(input('b:' ))
+side_3 = int(input('c:' ))
+
+type_of_triangle = ""
+
+if side_1 == side_2 and side_1 == side_3:
+    type_of_triangle = "equalateral"
+elif side_1 != side_2 and side_2 != side_3 and side_3 != side_1 :
+    type_of_triangle = "scalene"
+elif side_1 == side_2 or side_2 == side_3 or side_3 == side_1:
+    type_of_triangle = "isosceles"
+    
+print(f"A triangle with sides of a:{side_1}, b:{side_2} & c:{side_3} is a {type_of_triangle} triangle")
