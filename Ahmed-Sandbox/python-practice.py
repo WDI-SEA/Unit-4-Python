8c719b556ea0a00dd402b7d7592ce48b40d096b2
Ahmed Al-Thawadi <ahmed9althawadi@gmail.com>
2022-11-22 09:16:45 +0300

    add sandbox

---
+++ Ahmed-Sandbox/python-practice.py
@@ -0,0 +1,93 @@
+# # state = "Tokyo"
+
+# # year = 1990
+
+# # message = f"{state} in {year} was a great year"
+
+# # print(message)
+
+
+# # split_str = "ace of spades".split("a")
+
+# # print(split_str)
+
+# # alphabeat = "abcde"
+
+# # print(list(alphabeat))
+
+
+# # index_of_x = "abcdeqqqxxzz".replace("z", "a")
+
+# # print(index_of_x)
+
+# # print(len("eggs an cream"))
+
+
+# string = "ABCDEFGHIJKLMNOPQRSTUVWQXYZ"
+
+# print(string[-2])
+
+
+# num = 25
+# msg = "There are " + str(num) + " tacos"
+
+# print(msg)
+
+# # con = int(0xa5, 16)
+
+# # print(0xa5)
+
+
+
+
+"""
+If green is entered, print the message Go!
+If yellow is entered, print the message Slow Down!
+If red is entered, print the message Stop!
+If anything else is entered, print the message Bogus!
+"""
+
+# color = input('Enter "green", "yellow", "red": ').lower()
+# print(f'The user entered {color}')
+
+# if color == "green":
+#     print("Go!")
+# elif color == "yellow":
+#     print("Slow Down!")
+# elif color == "red":
+#     print("Stop!")
+# else:
+#     print("Bogus!")
+
+
+
+
+# names = ['Ahmed', 'Josh', 'Alex', 'Dio']
+
+# for name in names:
+#     print(name)
+
+# color = " "
+
+# while color.lower() != 'quit':
+
+#     color = input('Enter "green", "yellow", "red": ').lower()
+
+#     print(f'The user entered {color}')
+
+#     if color == "green":
+#         print("Go!")
+#     elif color == "yellow":
+#         print("Slow Down!")
+#     elif color == "red":
+#         print("Stop!")
+#     else:
+#         print("Bogus!")
+
+
+# for num in range(2, 11, 4):
+#     print(num)
+
+nums = list(range(5, 0, -1))
+
+print(nums)
