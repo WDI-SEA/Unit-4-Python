9dbd36af17258c34f98bce46f6648e3ef27c6a13
Sharifaaz <sharifaalzayed96@gmail.com>
2022-11-23 11:21:08 +0300

    Pulling the last update

---
+++ python-containers/where_my_things_are.py
@@ -0,0 +1,19 @@
+# Define a Python dictionary named where_my_things_are containing a few items where:
+
+# the keys are things you have, and
+# the values are the locations you keep those things.
+# Write a for loop that iterates over the items in the dictionary and prints each one as
+# My [thing] is kept [location]
+
+where_my_things_are = {
+   
+    'car key':'On my rooms nightstand inside a container',
+    'Memories box':'On my desktop',
+    'Jewelleries':'my secret zone'
+
+}
+
+print(where_my_things_are)
+
+# where_my_things_are.items()
+
