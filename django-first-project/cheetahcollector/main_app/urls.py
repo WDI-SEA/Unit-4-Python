d2c04600794af0ea7764cd1c97694c523a494acc
Sharifaaz <sharifaalzayed96@gmail.com>
2022-11-28 21:21:34 +0300

    CheetahCollector is done

--- django-first-project/cheetahcollector/main_app/urls.py
+++ django-first-project/cheetahcollector/main_app/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 urlpatterns = [
 path('', views.home, name='home'),
+path('home/', views.home, name='home'),
 path('about/', views.about, name='about'),
-
+path('cheetahs/', views.cheetahs_index, name='index'),
+path('cheetahs/<int:cheetah_id>/', views.cheetahs_detail, name='detail'),
+path('cheetahs/create/', views.CheetahCreate.as_view(), name='cheetahs_create'),
+path('cheetahs/<int:pk>/update/', views.CheetahUpdate.as_view(), name='cheetahs_update'),
+path('cheetahs/<int:pk>/delete/', views.CheetahDelete.as_view(), name='cheetahs_delete'),
+path('cheetahs/<int:cheetah_id>/add_feeding/', views.add_feeding, name='add_feeding'),
 ]
